11f8245e91b0d281bc8cb929e27866edf15fa7d1
sofiasister <147341952+sofiasister@users.noreply.github.com>
2023-10-25 18:36:20 -0400

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,24 +1,18 @@
 def mergeSort(arr):
     if len(arr) > 1:
  
-         # Finding the mid of the array
         mid = len(arr)//2
- 
-        # Dividing the array elements
+
         L = arr[:mid]
  
-        # Into 2 halves
         R = arr[mid:]
  
-        # Sorting the first half
         mergeSort(L)
  
-        # Sorting the second half
         mergeSort(R)
  
         i = j = k = 0
  
-        # Copy data to temp arrays L[] and R[]
         while i < len(L) and j < len(R):
             if L[i] <= R[j]:
                 arr[k] = L[i]
@@ -28,7 +22,6 @@ def mergeSort(arr):
                 j += 1
             k += 1
  
-        # Checking if any element was left
         while i < len(L):
             arr[k] = L[i]
             i += 1
@@ -40,14 +33,12 @@ def mergeSort(arr):
             k += 1
  
  
-# Code to print the list
 def printList(arr):
     for i in range(len(arr)):
         print(arr[i], end=" ")
     print()
  
  
-# Driver Code
 if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]
     print("Given array is")
