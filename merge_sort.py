f5b21df6ecf92f05ef9175122a057f7a331f8dfb
sofiasister <147341952+sofiasister@users.noreply.github.com>
2023-10-27 15:36:59 -0400

    Refactored sorting algorithm

--- merge_sort.py
+++ merge_sort.py
@@ -1,18 +1,22 @@
-def mergeSort(arr):
-    if len(arr) > 1:
- 
-        mid = len(arr)//2
+from typing import List
 
+def mergeSort(arr: List[int]) -> None:
+    """
+    Sort the given list in ascending order using the merge sort algorithm.
+
+    Args:
+        arr (List[int]): The list of integers to be sorted.
+
+    Returns:
+        None
+    """
+    if len(arr) > 1:
+        mid = len(arr) // 2
         L = arr[:mid]
- 
         R = arr[mid:]
- 
         mergeSort(L)
- 
         mergeSort(R)
- 
         i = j = k = 0
- 
         while i < len(L) and j < len(R):
             if L[i] <= R[j]:
                 arr[k] = L[i]
@@ -21,24 +25,29 @@ def mergeSort(arr):
                 arr[k] = R[j]
                 j += 1
             k += 1
- 
         while i < len(L):
             arr[k] = L[i]
             i += 1
             k += 1
- 
         while j < len(R):
             arr[k] = R[j]
             j += 1
             k += 1
- 
- 
-def printList(arr):
+
+def printList(arr: List[int]) -> None:
+    """
+    Print the elements of the given list.
+
+    Args:
+        arr (List[int]): The list of integers to be printed.
+
+    Returns:
+        None
+    """
     for i in range(len(arr)):
         print(arr[i], end=" ")
     print()
- 
- 
+
 if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]
     print("Given array is")
@@ -46,4 +55,3 @@ if __name__ == '__main__':
     mergeSort(arr)
     print("\nSorted array is ")
     printList(arr)
- 
