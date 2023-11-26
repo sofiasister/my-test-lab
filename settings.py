304ac62130c03254d499a5b8dce7fc63828568b6
sofiasister <147341952+sofiasister@users.noreply.github.com>
2023-11-26 01:17:22 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,4 +4,9 @@ class Settings:
   READ_FROM_FILE: bool
   UPLOAD_TO_BUCKET: bool = True
   DOWNLOAD_FROM_BUCKET: bool = True
+  BASE_URL: str
+  SQLITE_URL: str
+  READ_FROM_FILE: bool
+  UPLOAD_TO_BUCKET: bool = True
+  DOWNLOAD_FROM_BUCKET: bool = True
   
